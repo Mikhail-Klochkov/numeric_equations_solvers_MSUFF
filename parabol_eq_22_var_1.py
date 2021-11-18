8615619231cb9fb9ec904b3ab14fc52c1cdf6aed
mklochkov <m.klo4koff@yandex.ru>
2021-11-18 15:59:09 +0300

    Correct eq22var1

--- parabol_eq_22_var_1.py
+++ parabol_eq_22_var_1.py
@@ -6,8 +6,8 @@ a = 1.
 M = 200
 N = 200
 
-a_lenght = 3
-b_lenght = 5
+a_lenght = 5
+b_lenght = 3
 
 def f_test(t, x):
     return 0.0
@@ -19,12 +19,16 @@ def bound_cond_right_test(t):
     return 0.0
 
 def init_equat_test(x):
-    if 0 <= x <= a_lenght/2:
-        return 2 * b_lenght/a_lenght
-    elif a_lenght/2 < x <= a_lenght:
-        return (2 * b_lenght / a_lenght) * (a_lenght - x)
-    else:
-        assert False
+    arr_ans = np.zeros((x.shape[0], ))
+    for idx in range(arr_ans.shape[0]):
+        x_ = x[idx]
+        if 0 <= x_ <= a_lenght/2:
+            arr_ans[idx] =  2 * b_lenght/a_lenght
+        elif a_lenght/2 < x_ <= a_lenght:
+            arr_ans[idx] = (2 * b_lenght / a_lenght) * (a_lenght - x)
+        else:
+            assert False
+    return arr_ans
 
 def f(t, x):
     return np.exp(t*x)
@@ -42,19 +46,17 @@ plot_graph = True
 
 
 def init_and_bound_cond(y, x_range, t_range, test=False):
-    for idx in range(x_range.shape[0]):
-        if test:
-            y[0, idx] = init_equat_test(x_range[idx])
-        else:
-            y[0, idx] = 0
+    if test:
+        y[0, :] = init_equat_test(x_range)
+    else:
+        y[0, :] = 0
     # initialize right and left conditions
-    for jdx in range(t_range.shape[0]):
-        if test:
-            y[jdx, 0] = bound_cond_left_test(t_range[jdx])
-            y[jdx, -1] = bound_cond_right_test(t_range[jdx])
-        else:
-            y[jdx, 0] = bound_cond_left(t_range[jdx])
-            y[jdx, -1] = bound_cond_right(t_range[jdx])
+    if test:
+        y[:, 0] = bound_cond_left_test(t_range)
+        y[:, -1] = bound_cond_right_test(t_range)
+    else:
+        y[:, 0] = bound_cond_left(t_range)
+        y[:, -1] = bound_cond_right(t_range)
     return y
 
 
@@ -74,19 +76,19 @@ def solve(test):
         alpha[1] = 0
         beta[1] = 0
         for idx in range(1, x_range.shape[0] - 1):
-            alpha[idx + 1] = 1 / (2 + 1 / A - beta[idx])
+            alpha[idx + 1] = 1 / (2 + 1 / A - alpha[idx])
             if test:
                 f_var = f_test(t_range[jdx] + tau / 2, x_range[idx])
             else:
                 f_var = f(t_range[jdx] + tau / 2, x_range[idx])
 
-            beta[idx + 1] = (beta[idx] + y[jdx, idx] / A + tau * f_var) / (2 + 1 / A - beta[idx])
+            beta[idx + 1] = (beta[idx] + y[jdx, idx] / A + tau * f_var) / (2 + 1 / A - alpha[idx])
         # x_range.shape[0] = N+1 | N-1, N-2, ... 0 | where N=201 (199, 198, 197 .. 0)
         if test:
             f_var = f_test(t_range[jdx] + tau / 2, x_range[N])
         else:
             f_var = f(t_range[jdx] + tau / 2, x_range[N])
-        y[jdx + 1, N] = (beta[N] + y[jdx, N] / A + tau * f_var) / (2 + 1 / A - beta[N])
+        y[jdx + 1, N] = (beta[N] + y[jdx, N] / A + tau * f_var) / (2 + 1 / A - alpha[N])
         for i, idx in enumerate(reversed(range(x_range.shape[0] - 1))):
             y[jdx + 1, idx] = alpha[idx + 1] * y[jdx + 1, idx + 1] + beta[idx + 1]
     # visualize graph
@@ -108,5 +110,5 @@ def solve(test):
 
 
 if __name__ == '__main__':
-    test_use = False
+    test_use = True
     solve(test_use)
